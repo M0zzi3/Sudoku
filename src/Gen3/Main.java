6d952ddb5a239d2608b9b7e52b6da95427c4ec66
M0zzi3 <notmozzi3>
2021-11-25 19:28:17 +0100

    Test commit 2

--- src/Gen3/Main.java
+++ src/Gen3/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code her
         System.out.println("tets1");
+        //asfdasfadf
+        System.out.println("fsafasf");
     }
 }
